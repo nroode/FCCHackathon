4fc75d67867f0889e61e0c2f4083384a5dfd802b
Natalie Roode <nroode@gmail.com>
2018-08-29 11:33:01 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>I'm Natalie Roode. I'm a consumer insights researcher turned front-end developer.</p>
+    <p>I'm excited to participate in a hackathon.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
